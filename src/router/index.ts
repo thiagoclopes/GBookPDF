1abd7d74adc41ee1851fcedd76679cc260a55d61
natanmorais23 <natanbastosdemorais7@gmail.com>
2024-09-21 13:52:12 -0300

    creating login

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AuthLayout from '@/pages/auth/_layouts/auth'
 import SignIn from '@/pages/auth/sign-in'
+import Login from '@/views/Login.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,9 +13,9 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue')
+      path: '/login',
+      name: 'login',
+      component: Login
     },
     {
       path: '/',
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-import CounterButton from '@/components/CounterButton.tsx';
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
-    <CounterButton />
+    
   </main>
 </template>
